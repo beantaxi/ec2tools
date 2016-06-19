e726bc100fa113fac1968b99f7dcbf0c5840398a
Bean Taxi <beantaxi@gmail.com>
2016-06-18 22:07:59 -0500

    v0.4: Added findA

---
+++ ec2tools/findA.py
@@ -0,0 +1,21 @@
+from . import allThe
+from . import api
+
+def Instance (*, name=None, hostname=None, ip=None):
+	data = allThe.Instances()
+	if name:
+		hits = [o for o in data if api.getName(o) == name]
+	elif ip:
+		hits = [o for o in data if o.public_ip_address == ip]
+	elif hostname:
+		hits = [o for o in data if o.public_dns_name == hostname]
+	else:
+		raise Exception('Instance() called with no arguments')
+	if hits:
+		if len(hits) > 1:
+			raise Exception("Multiple instances found")
+		else:
+			instance = hits[0]
+	else:
+		instance = None
+	return instance
