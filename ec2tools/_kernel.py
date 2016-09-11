601be9ca4f030460255178898cd79e90156073ce
Bean Taxi <beantaxi@gmail.com>
2016-09-10 23:49:05 -0500

    Added Glacier support

--- ec2tools/_kernel.py
+++ ec2tools/_kernel.py
@@ -3,9 +3,10 @@ import boto3
 # Shared variables
 cli = boto3.client("ec2")
 factory = boto3.resource("ec2")
+glacier = boto3.resource("glacier")
 iam = boto3.resource("iam")
 
-def getCurrentOwnerId ():
+def getAccountId ():
 	currentUser = iam.CurrentUser()
 	arn = currentUser.arn
 	ownerId = arn.split(':')[4]
@@ -18,4 +19,3 @@ def toList (*args):
     if len(args) == 1 and isinstance(args[0], list):
         args = args[0]
     return args
-
