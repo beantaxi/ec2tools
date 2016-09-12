7d30e96b9f61d26632b530e7ef710e3b1ab8eb63
Bean Taxi <beantaxi@gmail.com>
2016-09-12 15:14:55 -0500

    glacier

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ setuptools.setup(name='ec2tools',
                  version='0.4.1',
                  url='https://github.com/beantaxi/ec2tools',
                  install_requires=['boto3'],
-                 package_dir={'ec2tools', 'ec2tools'})
+                 package_dir={'ec2tools': 'ec2tools'})
