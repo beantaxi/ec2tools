c81f279dab9edc1492873f2d315f83ab41c20016
Bean Taxi <beantaxi@gmail.com>
2016-09-13 13:05:53 -0500

    glacier

--- ec2tools/createA.py
+++ ec2tools/createA.py
@@ -1,4 +1,11 @@
 from ._kernel import cli
+from ._kernel import glacier
+
+
+def Vault (name):
+	vault = glacier.create_vault(vaultName=name)
+	return vault
+
 
 def Volume (*, size, availabilityZone, volumeType):
 	resp = cli.create_volume(Size=size, AvailabilityZone=availabilityZone, VolumeType=volumeType)
