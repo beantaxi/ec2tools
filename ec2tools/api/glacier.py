b3c37ef07fb9dae34f35a93d113933fa1769328e
Bean Taxi <beantaxi@gmail.com>
2016-09-13 20:49:28 -0500

    glacier

--- ec2tools/api/glacier.py
+++ ec2tools/api/glacier.py
@@ -2,8 +2,15 @@ import boto3
 import botocore.utils
 import io
 import math
+import numbers
 import os.path
 from ec2tools import getA
+
+ONE_MEG = 1<<20
+ONE_GIG = 1<<30
+MIN_CHUNK = ONE_MEG
+MAX_CHUNK = 4*ONE_GIG
+
 
 def genChunk (f, chunk_size):
 	# Create buffer
@@ -24,11 +31,6 @@ def genChunk (f, chunk_size):
 
 #######################################################################
 #
-# The rules:
-#
-#   Minimum chunk size = 1M
-#   Maximum chunk size = 4G
-#   Chunk size must be 1M, 2M, 4M, 8M etc: "a meg times a power of 2"
 #   We want 100 chunks or less
 #
 # So we want the smallest chunk size, where 
@@ -53,15 +55,10 @@ def genChunk (f, chunk_size):
 #   
 #    chunkSize = 2^n*1M
 #
-#  gives us our chunksize. Which we then need to clip to the Min and Max values.
+#  gives us our chunkSize. Which we then need to clip to the Min and Max values.
 #  Convert it to int, and we're good to go
 #
 def getChunkSize (totalSize):
-	ONE_MEG = 1<<20
-	ONE_GIG = 1<<30
-	MIN_CHUNK = ONE_MEG
-	MAX_CHUNK = 4*ONE_GIG
-
 	k = ONE_MEG*100
 	x = totalSize/k
 	y = math.log(x, 2)
@@ -72,14 +69,34 @@ def getChunkSize (totalSize):
 	return chunkSize
 
 
+# The rules:
+#
+#   Minimum chunk size = 1M
+#   Maximum chunk size = 4G
+#   Chunk size must be 1M, 2M, 4M, 8M etc: "a meg times a power of 2"
+#
+def validateChunkSize (chunkSize):
+	if not isinstance(chunkSize, numbers.Number) or chunkSize-int(chunkSize) != 0:
+		raise Exception("{} must a number, and convertible to an integer".format(chunkSize))
+	chunkSize = int(chunkSize)
+	if chunkSize < MIN_CHUNK or chunkSize > MAX_CHUNK:
+		raise Exception("{} must be between {} or {}".format(chunkSize, MIN_CHUNK, MAX_CHUNK))
+	if not math.log(chunkSize/ONE_MEG, 2).is_integer():
+		raise Exception("{} must be 1M times a power of 2 (1M, 2M, 4M, 8M, etc)".format(chunkSize))
+	return chunkSize	
+
+
 def uploadLargeFile (path, vaultName, archiveName=None, chunkSize=None):
-	# Validate that file exists
-	# Validate value exists
-	# Validate chunksize is legit
+		
+	if not os.path.exists(path): raise Exception("{} does not exist".format(path))	# Validate that file exists
+	if not os.path.isfile(path): raise Exception("{} is not a file".format(path))    # Validate it's a file
+	fileSize = os.path.getsize(path)
+	if filesize < MIN_SIZE: raise Exception("File size must be >= {}".format(MIN_SIZE))   # Validate the file is not too small
+	# Validate chunkSize is legit
+	# Validate vault exists
 	if not archiveName:
 		archiveName = os.path.basename(path)
 	if not chunkSize:
-		fileSize = os.path.getsize(path)
 		chunkSize = getChunkSize(fileSize)
 	vault	= getA.Vault(vaultName)
 	multi = vault.initiate_multipart_upload(archiveDescription=archiveName, partSize=str(chunkSize))
