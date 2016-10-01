882b620ea17619ddb1a6f4f6f0f2e171c5102be5
Bean Taxi <beantaxi@gmail.com>
2016-09-30 19:49:43 -0500

    fix to api

--- ec2tools/api/lifecycle.py
+++ ec2tools/api/lifecycle.py
@@ -1,4 +1,5 @@
 from ec2tools._kernel import cli 
+from ec2tools.api import nameInstance
 
 def getStatus (o):
 	status = o.state['Name']
--- publish.sh
+++ publish.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [[ -z SSH_AUTH_SOCK || -z SSH_AGENT_PID ]]; then
+	>&2 echo "Please run ssh-agent and then rerun this script"
+	exit 1
+fi
+
 git commit -am "$1" && git push; python3 setup.py clean sdist upload -r sparktools
