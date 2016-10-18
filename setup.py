aa5f47600852ba36219dcf32ecce949a2be56818
Bean Taxi <beantaxi@gmail.com>
2016-10-17 23:33:35 -0500

    Revved

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 setuptools.setup(name='ec2tools',
-                 version='0.4.1',
+                 version='0.4.2',
                  url='https://github.com/beantaxi/ec2tools',
                  install_requires=['boto3'],
                  packages=setuptools.find_packages())
