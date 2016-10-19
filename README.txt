6c19ddf3307763942efdf19465020da5d42e3fb9
Bean Taxi <beantaxi@gmail.com>
2016-10-19 13:39:38 -0500

    small changes

---
+++ README.txt
@@ -0,0 +1,8 @@
+ec2tools
+========
+
+boto is great. But boto isn't for every day use by humans. Boto is a low-level interface, practically assembly language.
+
+Why are you using assembly language?
+
+ec2tools is a higher-level library, with useful abstractions and a rich API. So you can use AWS like a human.
--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ import setuptools
 
 
 setuptools.setup(name='ec2tools',
+                 description='Handy tools for using boto as intended.',
+                 keywords='boto boto3 awsi ec2',
                  version='0.4.2',
                  url='https://github.com/beantaxi/ec2tools',
+                 author='Chris Lalos',
+                 author_email='chris.lalos@gmail.com',
                  install_requires=['boto3'],
-                 packages=setuptools.find_packages())
+                 packages=['ec2tools', 'ec2tools.api'],
+                 package_dir={'': '.'})
+#                 packages=setuptools.find_packages(exclude=['tests']))
